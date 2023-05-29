5b2e6ab094d840065f6eed21f45d4f4fb4d2f239
Nikita Bokov <nekit123333@yandex.ru>
2023-05-29 16:19:35 +0300

    -Доработал класс Card, забил api в качестве параметра и прописал всю логику отрисовки после нажатия на слушатели, а в index.js вставил в метод, который отрисовывает карточки и нужным параметром вставил api,

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
 
-
 export default class Card{
-	constructor(	data, selector ){
+	constructor(	data, selector, renderer ){
 		this._cardLink = `${data.link}`;
 		this._cardTitle = `${data.name}`;
 		this._cardLikeSum = `${data.likes.length}`;
+		this._cardLikes = data.likes;
 		this._selector = `${selector}`;
 		this._cardId = `${data._id}`;
-		this._ownerId = `${data.owner._id}`
+		this._ownerId = `${data.owner._id}`;
+		this._renderer = renderer;
 	}
 
 	_getElement() {
@@ -20,52 +21,74 @@ export default class Card{
 		return cardCloneElement;
 	}
 
-	generate(idPerson) {
+	generate(idPerson, url) {
 		this._element = this._getElement();
 
 		this._element.querySelector('.card__close').disabled = this._ownerId !== idPerson;
 
-		this._setEventListeners();
+		this._setEventListeners(url);
 
 		this._element.querySelector('.card__image').src = this._cardLink;
 		this._element.querySelector('.card__image').alt = this._cardTitle;
 		this._element.querySelector('.card__title').textContent = this._cardTitle;
 		this._element.querySelector('.card__like-sum').textContent = this._cardLikeSum;
 
+		this._cardLikes.forEach((item) => {
+			if(item._id === idPerson){
+				this._element.querySelector('.card__like').classList.add('card__like_active');
+			}
+		});
+
 		return this._element;
 	}
 
-	_setEventListeners() {
+	_setEventListeners(url) {
 		this._element.querySelector('.card__image').addEventListener('click', () => {
-
+			this._clickOpenPopup();
 		});
 		
 		this._element.querySelector('.card__like').addEventListener('click', (evt) => {
-			this._clickLike();
+			this._clickLike(evt, url);
 		});
 
-		this._element.querySelector('.card__close').addEventListener('click', () => {
-			this._clickDelCard();
-		});
+		this._element.querySelector('.card__close').addEventListener('click', () => this._clickDelCard(url));
 
 	}
 
-	_clickLike() {
+	_clickLike(evt, url) {
 		const itemLike = this._element.querySelector('.card__like');
 		if(itemLike.classList.contains('card__like_active')){
-			itemLike.classList.remove('card__like_active');
+			url.getAddLike(this._cardId, 'DELETE')
+			.then((item) => {
+				evt.target.classList.remove('card__like_active');
+				this._element.querySelector('.card__like-sum').textContent = item.likes.length;
+			})
+			.catch((error) => {
+				return console.log(error);
+			});
 		} else {
-			itemLike.classList.add('card__like_active');
+			url.getAddLike(this._cardId, 'PUT')
+			.then((item) => {
+				evt.target.classList.add('card__like_active');
+				this._element.querySelector('.card__like-sum').textContent = item.likes.length;
+			})
+			.catch((error) => {
+				return console.log(error);
+			});
 		}
 	}
 
-	_clickDelCard() {
-		const deleteItem = this._element.querySelector('.card__close').closest('.card');
-		deleteItem.remove();
+
+	_clickDelCard(url) {
+		url.getDeleteCard(this._cardId)
+		.then(() => {
+			this._element.querySelector('.card__close').closest('.card').remove();
+		})
+		.catch(err => console.log(err));
 	}
 
 	_clickOpenPopup(){
-		
+		return this._renderer();
 	}
 	
 }
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,20 +1,34 @@
 export default class UserInfo{
-	constructor({ name, avatar, about, _id }){
+	constructor({ name, avatar, about}){
 		this._name = name;
 		this._about = about;
 		this._avatar = avatar;
-		this._id = _id;
 	}
 
-	getUserInfo({names, aboutJob, avatars}) {
-		names.textContent = this._name;
-		aboutJob.textContent = this._about;
-		avatars.src = this._avatar;
+	getUserInfo() {
+		this._element = document.querySelector('.profile');
+
+		this._element.querySelector('.profile-info__name').textContent = this._name;
+
+		this._element.querySelector('.profile-info__profession').textContent = this._about;
+
+		this._element.querySelector('.profile__image').src = this._avatar;
+
 		
 	}
 
-	setUserInfo() {
-
+	setUserInfo(api, {inputName, inputJob, inputUrl}) {
+		api.getSwapTextProfile(inputName, inputJob)
+		.then((res) => {
+			this._element.querySelector('.profile-info__name').textContent = res.name;
+			this._element.querySelector('.profile-info__profession').textContent = res.about;
+		})
+		.catch(err => console.log(err));
+		api.getSwapAvatar(inputUrl)
+		.then((res) => {
+			this._element.querySelector('.profile__image').src = res.avatar;
+		})
+		.catch(err => console.log(err));
 	}
 
 }
